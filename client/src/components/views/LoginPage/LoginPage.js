7f622a9da1a37b9c018b7502401099cc824cf583
leechungheon <leechung6311@gmail.com>
2025-01-12 18:37:52 +0900

    Implement: React router dom

---
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LoginPage() {
+  return (
+    <div>LoginPage 안녕하세요</div>
+  )
+}
+
+export default LoginPage
---
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RegisterPage() {
+  return (
+    <div>RegisterPage 안녕하세요</div>
+  )
+}
+
+export default RegisterPage
